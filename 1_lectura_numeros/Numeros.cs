564089ae3b0c2b5011b39d6f53d46ae17e726766
josue65390 <86178629+josue65390@users.noreply.github.com>
2021-06-23 00:12:29 -0600

    Se creo primer ejercicio lectura de numeros
    Aplicacion

---
+++ 1_lectura_numeros/Numeros.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1_lectura_numeros
+{
+    class Numeros
+    {
+        //Atributos 
+        //Arreglo
+        int[] almacen;
+        int numero, suma, mayor, menor;
+        //para considerar los puntos decimales
+        double promedio;
+        //------------Propiedades--------------
+
+        public int NUMERO { get { return numero; } set { numero = value; } }
+        public int SUMA { get { return suma; } set { suma = value; } }
+        public double PROMEDIO { get { return promedio; } set { promedio = value; } }
+        public int MAYOR { get { return mayor; } set { mayor = value; } }
+        public int MENOR { get { return menor; } set { menor = value; } }
+
+        //método
+        public void calcularLaSuma()
+        {
+            almacen = new int[10];
+            SUMA += NUMERO;
+        }
+        public void calcularPromedio()
+        {
+            almacen = new int[10];
+            PROMEDIO = SUMA / 10;
+        }
+        public void calcularElMayor()
+        {
+            almacen = new int[10];
+            for (int i = 0; i <= 10; i++)
+            {
+                almacen[i] = NUMERO;
+                if (almacen[i] == 0)
+                {
+                    MAYOR = NUMERO;
+                }
+                else if(i != 0)
+                {
+
+                    if(NUMERO >MAYOR)
+                    {
+                        MAYOR = NUMERO;
+                    }
+                }
+            }
+        }
+        public void calcularElMenor()
+        {
+            almacen = new int[10];
+            for (int i = 0; i <= 10; i++)
+            {
+                almacen[i] = NUMERO;
+                if (almacen[i] == 0)
+                {
+                  MENOR = NUMERO;
+                }
+                else if (i != 0)
+                {
+                    if (NUMERO < MENOR)
+                    {
+                        MENOR = NUMERO;
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ 1_lectura_numeros/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _1_lectura_numeros
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //se manda a llamar el objeto al metodo main de impresion a consola
+            Numeros objOrdenamiento = new Numeros();
+            //.......cambio de color.......
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(".....BIENVENIDO....");
+            Console.WriteLine(" Esta aplicacion le servira para hacer la sumatoria, el promedio, el numero mayor y el numero menor de 10 números ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(" Introduce los numeros");
+            for (int i = 0; i < 10; i++)
+            {
+                do
+                {
+                    Console.Write(" + " + (i + 1) + ": ");
+                    objOrdenamiento.NUMERO = int.Parse(Console.ReadLine());
+                }
+                //Aqui se madnara a imprimir el objeto creado
+                while (objOrdenamiento.NUMERO <= 0);
+                objOrdenamiento.calcularElMenor();
+                objOrdenamiento.calcularElMayor();
+                objOrdenamiento.calcularLaSuma();
+                objOrdenamiento.calcularPromedio();
+            }
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine(" La suma de los números es: " + objOrdenamiento.SUMA);
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine(" El promedio es: " + Convert.ToDouble(objOrdenamiento.PROMEDIO));
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine(" El numero mayor es: " + objOrdenamiento.MAYOR);
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine(" El numero menor es: " + objOrdenamiento.MENOR);
+
+        }
+    }
+}
+
