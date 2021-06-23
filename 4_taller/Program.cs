13b8e4e95f3a2dec6b664a5ecb76b0762afb276b
josue65390 <86178629+josue65390@users.noreply.github.com>
2021-06-23 00:19:34 -0600

    Ejercicio 4 taller aplicacion
    Se realizo con la ayuda de compañeros de escuela

---
+++ 4_taller/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace _4_taller
+{
+
+    /*
+    class datos
+    {
+        private int[] data;
+        private int indice, limite;
+        //métodos
+        /*.........................Datos.....................
+         * Este constructor requiere de un valor entero tamaño 
+         * para crear el espacio de la pila de enteros 
+         * que utilizaran para almacenar 
+         * --------------------------------------------------
+        public datos (int tamaño)
+        {
+            this.data = new int[tamaño];
+            this.indice = -1;
+            this.limite = tamaño;
+        }
+        //------------------------Avanzar/empujar----------------------
+        //El método recibe un dato y lo empuja (push) a la pila de datos 
+        public int Push(int data)
+        {
+            int exito = 0;
+            if (indice < this.limite - 1)
+                this.data[++indice] = data;
+            else exito = 1;
+            return exito;
+        }
+    }         
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Hello World! ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Este es un taller mecanico en el cual arreglamos tu auto" + "¡Vamos dejalo aqui!");
+            Console.ForegroundColor = ConsoleColor.White;
+            Taller taller = new Taller(5);// SE INSTANCIA CON 5 QUE ES LA MAX.
+            Console.WriteLine(taller.EspacioDisponible());
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Estos son los espacios disponible");
+            Console.WriteLine("Si ingresa 'S' el programa cerrara");
+            char ans;
+            char salir = 'f';
+            while (salir != 'S')
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("¿Que deseas hacer?");
+                Console.WriteLine("A)Meter\nB)sacar\n");
+                Console.WriteLine("Seleccione su inciso: ");
+                ans = char.ToUpper(Convert.ToChar(Console.ReadLine()));
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Clear();
+                switch (ans)
+                {
+                    case 'A':
+                        {
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+                            Console.WriteLine("Ha seleccionado meter");
+                            Console.Write("Ingrese su placa: ");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            string placa = Convert.ToString(Console.ReadLine());
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+                            Console.Write("Ingrese su nombre: ");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            string nombre = Convert.ToString(Console.ReadLine());
+                            if (taller.Meter(placa, nombre) == false)
+                            {
+                                Console.WriteLine("Error tabla llena");
+                                Console.ReadKey();
+                            }
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+                            Console.Clear();
+                            Console.WriteLine("Se ha actualizado la tabla");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.WriteLine(taller.EspacioDisponible());
+                            Console.ReadKey();
+                            break;
+                        }
+                    case 'B':
+                        {
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+                            Console.WriteLine("Ha seleccionado sacar");
+                            if (taller.Sacar() == false)
+                            {
+                                Console.WriteLine("Error tabla vacia");
+                                Console.ReadKey();
+                            }
+                            Console.WriteLine("Se ha actualizado la tabla");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.WriteLine(taller.EspacioDisponible());
+                            Console.ReadKey();
+                            break;
+                        }
+                }
+                Console.Clear();
+                Console.WriteLine("¿Desea salir? presione S o presione cualquier tecla para continuar");
+                salir = char.ToUpper(Convert.ToChar(Console.ReadLine()));
+                Console.Clear();
+            }
+        }
+    }
+}
---
+++ 4_taller/Taller.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _4_taller
+{
+    class Taller
+    {
+            //-------------------------- ATRIBUTOS --------------------------------
+            private string[,] carros;
+            private List<string> carrosAcumulados;
+            private List<int> monto;
+            private int indice;
+            private int indiceS;
+            //-------------------------- CONSTRUCTOR -----------------------------
+            public Taller(int n)
+            {
+                N = n;
+                //-- NUMERO DE CAPACIDAD MAXIMA DE AUTOS DENTRO DEL TALLER
+                Carros = new string[N, 2];
+                Monto = new List<int>();
+                CarrosAcumulados = new List<string>();
+                IndiceS = 0;
+                Indice = 0;
+                //-- AYUDARA A CONTAR PARA VER LA DISPONIBILIDAD DE ESPACIOS
+                for (int rows = 0; rows < N; rows++)
+                //-- PONE TODO LOS ESPACIOS SIN INFO PARA VER QUE ESTA DISPONIBLE
+                {
+                    for (int columns = 0; columns < 2; columns++)
+                    {
+                        Carros[rows, columns] = " - ";
+                    }
+                }
+            }
+            //------------------------- METODOS ---------------------------
+            public Boolean Meter(string placas, string nombreDueño)
+            {
+                if (Indice < N)
+                //-- METE UN AUTO AL TALLER
+                {
+                    for (int i = 0; i < N; i++)
+                    {
+                        if (Carros[i, 0] == " - ")
+                        {
+                            Indice = i;
+                            break;
+                        }
+                    }
+                    Carros[Indice, 0] = placas;
+                    Carros[Indice, 1] = nombreDueño;
+                    string value = $"{placas}, {nombreDueño}";
+                    CarrosAcumulados.Add(value);
+                    return true;
+                }
+                return false;
+            }
+            public Boolean Sacar()//-- SACA UN AUTO DEL TALLER
+            {
+                if (Indice - 1 >= 0)
+                {
+                    for (int i = 0; i < N; i++)
+                    {
+                        if (Carros[i, 0] != " - ")
+                        {
+                            Carros[i, 0] = Carros[i + 1, 0];
+                            Carros[i, 1] = Carros[i + 1, 1];
+                        }
+                    }
+                    Carros[N - 1, 0] = " - ";
+                    Carros[N - 1, 1] = " - ";
+                    return true;
+                }
+                return false;
+            }
+            public string EspacioDisponible()//-- DEVUELVE EL ARREGLO DE CARROS QUE ESTAN
+            {
+                String ans = "";
+                for (int rows = 0; rows < N; rows++)//-- MUESTRA ESPACIOS DISPONIBLES
+                {
+                    ans += carros[rows, 0] + ", ";
+                    ans += carros[rows, 1] + "\n";
+                }
+                return ans;
+            }
+            public void Cobrar(int monto)//-- COBRA EL DINERO Y LO GUARDA EN UNA LISTA
+            {
+                Monto.Add(monto);
+            }
+            public int[] GetMonto()//-- DEVUELVE LA LISTA DEL MONTO EN FORMA DE ARREGLO
+            {
+                int[] AMonto = Monto.ToArray();
+                return AMonto;
+            }
+            public string[] GetCarrosAcumulados()//-- DEVUELVE LA LISTA DE CARROS ACUMULADOS EN UN ARREGLO
+            {
+                string[] ACarrosAcumulados = CarrosAcumulados.ToArray();
+                return ACarrosAcumulados;
+            }
+            //------------------------- PROPIEDADES ----------------------
+            public string[,] Carros
+            {
+                get => carros;
+                set => carros = value;
+            }
+            public int N
+            {
+                get;
+                set;
+            }
+            public List<int> Monto
+            {
+                get => monto;
+                set => monto = value;
+            }
+            public int Indice
+            {
+                get => indice;
+                set => indice = value;
+            }
+            public List<string> CarrosAcumulados
+            {
+                get => carrosAcumulados;
+                set => carrosAcumulados = value;
+            }
+            public int IndiceS
+            {
+                get => indiceS;
+                set => indiceS = value;
+            }
+    }
+}
